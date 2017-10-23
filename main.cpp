c0361de42ba8886762afcccd12d2c088668a4124
poljkee2010 <31631937+poljkee2010@users.noreply.github.com>
2017-10-24 00:05:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,9 +16,9 @@
  		if (fs::exists(address) == 0) // Проверка существования файла по введенному адресу
  			throw runtime_error("File does not find!");
 
- 		const fs::path filePath = address; // Указываем путь к файлу file.dat
- 		const fs::path textFilename = filePath.filename(); // Получаем имя файла file.dat
-		auto  writeTimePoint = fs::last_write_time(filePath); 			// Получаем время последнего изменения файла file.dat
+ 		const fs::path filePath = address; // Указываем путь к файлу 
+ 		const fs::path textFilename = filePath.filename(); // Получаем имя файла
+		auto  writeTimePoint = fs::last_write_time(filePath); 			// Получаем время последнего изменения файла 
   		time_t cftime = decltype(writeTimePoint)::clock::to_time_t(writeTimePoint);   // Переделаем данные времени в строку для вывода
 
   		//ofstream - класс, используемый для записи данных в файл
